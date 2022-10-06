b380d1d4adb63566edf4d54e907945e2be2b92ae
explosion-mental <explosion0mental@gmail.com>
2022-10-06 17:14:18 -0500

    main skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,75 @@
+use std::env;
+use std::process::exit;
+
+static VERSION: u32 = 0;
 static CPUS: u32 = 8;
 
 fn main() {
-    for i in 1..6 {
-        println!("{}", i);
+    let argv: Vec<String> = env::args().collect();
+    let argc = argv.len();
+
+    if argv.len() == 1 {
+        println!("No arguments");
+        exit(1);
     }
-    info();
+
+	for i in 1..argc {
+        if argv[i] == "-v" || argv[i] == "--version" {
+            println!("racf-{}", VERSION);
+            exit(0);
+        } else if argv[i] == "-l" || argv[i] == "--list" { /* stats about the system */
+            info();
+            exit(0);
+        } else if argv[i] == "-t" || argv[i] == "--enable-turbo" { /* turbo on */
+            turbo(1);
+            exit(0);
+        } else if argv[i] == "-T" || argv[i] == "--disable-turbo" { /* turbo off */
+            turbo(0);
+            exit(0);
+        } else if argv[i] == "-r" || argv[i] == "--run-once" { /* turbo off */
+			run();
+			exit(0);
+        } else if argv[i] == "-b" || argv[i] == "--daemon" { /* turbo off */
+			daemonize();
+		} else if i + 1 == argc {
+			usage();
+		/* these options take one argument */
+		} else if argv[i] == "-g" || argv[i] == "--governor" { /* set governor */
+			//setgovernor(argv[i + 1]);
+            //no ++ or -- huh?
+			exit(0);
+		} else {
+			usage();
+        }
+
+    }
 }
 
 fn info() {
-
 	println!("Cores: {}", CPUS);
 	println!("AC adapter status: {}", "ischarging");
 	println!("Average system load: {}", "avgload");
 	println!("System temperature: {} °C", "avgtemp");
 }
+
+fn run() {
+    println!("run()");
+}
+
+fn daemonize() {
+    println!("daemonize()");
+}
+
+fn turbo(i: i32) {
+    println!("turbo()");
+}
+
+fn setgovernor(gov: String) {
+    println!("setgovernor()");
+}
+
+fn usage() {
+    println!("usage()");
+    exit(0);
+}
+
