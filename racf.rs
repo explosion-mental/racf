e2c59e788a8d08daac554367d826d5a65170b2ab
explosion-mental <explosion0mental@gmail.com>
2023-01-24 12:46:44 -0500

    do not run if there is an instance alrd running

--- racf.rs
+++ racf.rs
@@ -110,6 +110,21 @@ impl Config {
 }
 
 fn main() {
+    use sysinfo::{ProcessExt, System, SystemExt, get_current_pid};
+    //XXX check temperature with sysinfo¿
+
+    let s = System::new_all();
+    let ppid = match get_current_pid() {
+        Ok(o) => o,
+        Err(e) => die!("Failed to get pid: {}", e),
+    };
+
+    for process in s.processes_by_exact_name("racf") {
+        if process.pid() != 0.into() && process.pid() != ppid {
+            die!("racf is already running ({}).", process.pid());
+        }
+    }
+
     match cli_flags() {
         Ok(()) => (),
         Err(e) => die!("{}", e),
