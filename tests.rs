3fabf4585140f679152da8a3d1f86c3dc673ec70
explosion-mental <explosion0mental@gmail.com>
2023-02-28 18:52:47 -0500

    [test]: add invalid_turbo()

--- tests.rs
+++ tests.rs
@@ -24,3 +24,32 @@ governor = \"erformance\" ") // <-- should be "performance"
         return;
     };
 }
+
+#[test]
+fn invalid_turbo() {
+    use crate::MainE;
+    use crate::Config;
+
+    let file: Config = toml::from_str(
+// turbo values should be 'auto' - 'always' - 'never'
+"
+[ac]
+turbo = \"aut\"
+mincpu = 30.0
+mintemp = 70
+interval = 60
+governor = \"performance\"
+[battery]
+turbo = \"auto\"
+mincpu = 30.0
+mintemp = 70
+interval = 60
+governor = \"performance\" ")
+        .expect("NeverFailing");
+
+    let f = file.validate();
+    if f.is_ok() { // Should error out
+        dbg!(&f);
+        panic!("Parsed an invalid governor.\n'{:?}'", f);
+    };
+}
